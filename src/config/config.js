afdb5e007f56a993dce68b16587e18eef0d093aa
MincheolCha <mc.cha@greenlabs.co.kr>
2022-12-02 21:42:01 +0900

    sequelize init , mysql 연결

--- src/config/config.js
+++ src/config/config.js
@@ -2,3 +2,26 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+const sequelizeMysqlConfig = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: "mysql",
+  port: 3306,
+  dialectOptions: {
+    bigNumberStrings: true,
+  },
+};
+
+module.exports = {
+  // 서버 설정
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+
+  // Mysql (Sequelize) 설정
+  development: sequelizeMysqlConfig,
+  test: sequelizeMysqlConfig,
+  production: sequelizeMysqlConfig,
+};
